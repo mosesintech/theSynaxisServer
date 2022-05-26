fbdea97edf06a89e465ca03c40204159eb35795f
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2022-05-26 18:01:06 -0500

    add Saint Type for GraphQL

---
+++ server/__tests__/api/types.spec.ts
@@ -0,0 +1,28 @@
+import { GraphQLID, GraphQLString, GraphQLBoolean, GraphQLInt } from 'graphql';
+import SaintType from '../../api/types';
+
+describe('GraphQL Types', () => {
+  test('Saint Type', () => {
+    const fields = SaintType.getFields();
+    expect(SaintType.name).toEqual('SaintType');
+    expect(fields.id.type).toEqual(GraphQLID);
+    expect(fields.name.type).toEqual(GraphQLString);
+    expect(fields.life.type).toEqual(GraphQLString);
+    expect(fields.born.type).toEqual(GraphQLString);
+    expect(fields.died.type).toEqual(GraphQLString);
+    expect(fields.isBC.type).toEqual(GraphQLBoolean);
+    expect(fields.feastMonth.type).toEqual(GraphQLInt);
+    expect(fields.feastDate.type).toEqual(GraphQLInt);
+    expect(fields.isMartyr.type).toEqual(GraphQLBoolean);
+    expect(fields.isPatriarch.type).toEqual(GraphQLBoolean);
+    expect(fields.isBishop.type).toEqual(GraphQLBoolean);
+    expect(fields.isPriest.type).toEqual(GraphQLBoolean);
+    expect(fields.isDeacon.type).toEqual(GraphQLBoolean);
+    expect(fields.isMonk.type).toEqual(GraphQLBoolean);
+    expect(fields.isMarried.type).toEqual(GraphQLBoolean);
+    expect(fields.isMale.type).toEqual(GraphQLBoolean);
+    expect(fields.createdAt.type).toEqual(GraphQLString);
+    expect(fields.modifiedAt.type).toEqual(GraphQLString);
+    expect(fields.isDeleted.type).toEqual(GraphQLBoolean);
+  });
+});
---
+++ server/api/types.ts
@@ -0,0 +1,34 @@
+import {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLInt,
+} from 'graphql';
+
+const SaintType = new GraphQLObjectType({
+  name: 'SaintType',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    life: { type: GraphQLString },
+    born: { type: GraphQLString },
+    died: { type: GraphQLString },
+    isBC: { type: GraphQLBoolean },
+    feastMonth: { type: GraphQLInt },
+    feastDate: { type: GraphQLInt },
+    isMartyr: { type: GraphQLBoolean },
+    isPatriarch: { type: GraphQLBoolean },
+    isBishop: { type: GraphQLBoolean },
+    isPriest: { type: GraphQLBoolean },
+    isDeacon: { type: GraphQLBoolean },
+    isMonk: { type: GraphQLBoolean },
+    isMarried: { type: GraphQLBoolean },
+    isMale: { type: GraphQLBoolean },
+    createdAt: { type: GraphQLString },
+    modifiedAt: { type: GraphQLString },
+    isDeleted: { type: GraphQLBoolean },
+  }),
+});
+
+export default SaintType;
